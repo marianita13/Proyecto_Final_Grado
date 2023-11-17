ddc6383da221bd1cfc555db3784f44a9e1413181
marianita13 <marianitacero.1913@gmail.com>
2023-11-17 02:22:32 -0500

    feat: :zap: JS

--- Frontend/Assets/JS/Dashboard.js
+++ Frontend/Assets/JS/Dashboard.js
@@ -113,51 +113,114 @@ window.addEventListener('click', function (e) {
 	})
 })
 
+/*GRAPHIC ONE*/
 var dom = document.getElementById('content-data');
     var myChart = echarts.init(dom, null, {
-      renderer: 'canvas',
-      useDirtyRect: false
+    renderer: 'canvas',
+    useDirtyRect: false
     });
     var app = {};
     
     var option;
 
     option = {
-  title: {
-    text: 'Referer of a Website',
-    subtext: 'Fake Data',
-    left: 'center'
-  },
-  tooltip: {
-    trigger: 'item'
-  },
-  legend: {
-    orient: 'vertical',
-    left: 'left'
-  },
-  series: [
-    {
-      name: 'Access From',
-      type: 'pie',
-      radius: '50%',
-      data: [
-        { value: 580, name: 'Entregado' },
-        { value: 484, name: 'Pendiente' },
-        { value: 470, name: 'Rechazado' }
-      ],
-      emphasis: {
-        itemStyle: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 0, 0, 0.5)'
-        }
-      }
-    }
-  ]
+	title: {
+		text: 'Orders Status',
+		subtext: 'Database Data',
+		left: 'center',
+		top:'40px'
+	},
+	tooltip: {
+		trigger: 'item'
+	},
+	legend: {
+		orient: 'vertical',
+		left: '20px',
+		top:'40px'
+	},
+	series: [
+		{
+			name: 'Access From',
+			type: 'pie',
+			top: '100px',
+			radius: '50%',
+			data: [
+				{ value: 65, name: 'Entregado' },
+				{ value: 33, name: 'Pendiente' },
+				{ value: 32, name: 'Rechazado' }
+			],
+			emphasis: {
+				itemStyle: {
+				shadowBlur: 10,
+				shadowOffsetX: 0,
+				shadowColor: 'rgba(0, 0, 0, 0.5)'
+				}
+			}
+		}
+	]
 };
 
-    if (option && typeof option === 'object') {
-      myChart.setOption(option);
-    }
+if (option && typeof option === 'object') {
+myChart.setOption(option);
+}
 
-    window.addEventListener('resize', myChart.resize);
+window.addEventListener('resize', myChart.resize);
+
+/*GRAPHIC TWO*/
+
+var dom = document.getElementById('other-graphic');
+var myChart = echarts.init(dom, null, {
+renderer: 'canvas',
+useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+	tooltip: {
+		trigger: 'axis',
+		axisPointer: {
+			type: 'shadow'
+		}
+	},
+	legend: {
+		top:'10px'
+	},
+	grid: {
+		left: '3%',
+		top:'50px',
+		right: '4%',
+		bottom: '6%',
+		containLabel: true
+	},
+	xAxis: [
+		{
+			type: 'category',
+			data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug','Sep','Oct','Nov','Dec']
+		}
+	],
+	yAxis: [
+		{
+			type: 'value'
+		}
+	],
+	series: [
+		{
+			name: 'Orders',
+			color: '#FC3B56',
+			type: 'bar',
+			emphasis: {
+			focus: 'series'
+			},
+			data: [39,13,19,8,5,3,2,3,1,8,6,9]
+		}
+	]
+};
+
+if (option && typeof option === 'object') {
+	myChart.setOption(option);
+}
+
+window.addEventListener('resize', myChart.resize);
+
--- Frontend/Assets/JS/main.js
+++ Frontend/Assets/JS/main.js
@@ -9,3 +9,64 @@ sign_up_btn.addEventListener('click', () =>{
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 });
+
+
+
+function crearInfo(fileName, data) {
+    const filePath = `../../Assets/${fileName}`;
+    const fileData = JSON.parse(readFileSync(filePath));
+    fileData.data.push(data);
+    writeFileSync(filePath, JSON.stringify(fileData, null, 4));
+
+}
+
+
+function checkfile(filePath) {
+    try {
+        fs.readFileSync(filePath);
+        return true;
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+        console.error("El archivo no existe.");
+        } else {
+        console.error("Ha ocurrido un error al leer el archivo.");
+        }
+        return false;
+    }
+}
+
+
+/* LOGIN */
+
+const userlogin = document.querySelector("#user-login");
+const passwordlogin = document.querySelector("#password-login");
+
+
+/* SIGN UP */
+const user = document.querySelector("#user");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const buttonUp = document.querySelector("#Sign-Up")
+
+// Validate Login Form
+buttonUp.addEventListener('click',(evento) =>{
+    var diccUser = {}
+    evento.preventDefault();
+    var loginUser = user.value;
+    var emailUser = email.value;
+    var loginPass = password.value;
+    if (loginUser == "" || loginPass == "") {
+        alert("Please Fill All Fields!");
+        return false;
+    }
+    else{
+        data = {
+            "username": loginUser,
+            "password": loginPass,
+            "email": emailUser
+        }
+        diccUser.data = diccUser
+        crearInfo("user.json",data)
+        console.log(data);
+    }
+})
